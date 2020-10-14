3e2aa3df5a3ed9d166144d2cd4037f8167e93ced
madmax7896 <thegreatsagar123@gmail.com>
2020-10-14 19:26:40 +0000

    drivers: Cleanup a bit

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 SMP PREEMPT Wed Oct 14 19:03:02 UTC 2020"
+#define LINUX_COMPILE_BY "madmax"
+#define LINUX_COMPILE_HOST "instance-1"
+#define LINUX_COMPILER "Proton clang version 12.0.0 (https://github.com/llvm/llvm-project 1ee88e6efe4e578c064c2c3779ca104478dfe136)"
